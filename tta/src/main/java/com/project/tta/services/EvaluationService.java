dd0239b8abb5ad1b1e1895c7dd1c83d3cbe91b65
Ilya <ilyasichev2015@gmail.com>
2025-03-28 00:29:26 +0300

    evaluation realization and some fixes

--- tta/src/main/java/com/project/tta/services/EvaluationService.java
+++ tta/src/main/java/com/project/tta/services/EvaluationService.java
@@ -1,13 +1,19 @@
 package com.project.tta.services;
 
 import com.project.tta.services.interfaces.EvaluationInterface;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class EvaluationService implements EvaluationInterface {
+    private static final Logger log = LoggerFactory.getLogger(EvaluationService.class);
     private final TimeTableParser timeTableParser;
 
     public EvaluationService(TimeTableParser timeTableParser) {
@@ -23,6 +29,7 @@ public class EvaluationService implements EvaluationInterface {
         int grade = 0;
         grade += evaluateGaps(table);
         grade += evaluateStudyDays(table);
+        grade += evaluateLoadBalance(table);
         grade += evaluateDailyLoad(table);
         grade += evaluateLessonStartTime(table);
         grade += evaluateLessonEndTime(table);
@@ -33,16 +40,61 @@ public class EvaluationService implements EvaluationInterface {
 
     @Override
     public int evaluateGaps(String[][] table) {
-        return 0;
+        int result = 0;
+        for (String[] dayTable : table) {
+            int gap = 0;
+            var dayTableBool
+                    = Arrays.stream(dayTable).map(EvaluationService::isBlank).toArray(Boolean[]::new);
+            List<Integer> dayTableInt = new ArrayList<>();
+//            for (int i = 0; i < dayTableBool.length; i++) {
+//                if (dayTableBool[i]){
+//                    dayTableInt.add(i);
+//                }
+//            }
+            for (int i = 0; i < dayTableInt.size() - 1; i++) {
+                gap = dayTableInt.get(i + 1) - dayTableInt.get(i) - 1;
+            }
+            if (gap == 0) {
+                result += 1;
+            } else {
+                if (gap == 1) result -= 2;
+                if (gap > 1) result -= 5;
+            }
+        }
+        log.info("evaluate by gap and return {}", result);
+        return result;
     }
 
     @Override
     public int evaluateStudyDays(String[][] table) {
+        int studyDays = (int) Arrays.stream(table).map(EvaluationService::dayIsFree).count();
+        int result = switch (studyDays) {
+            case 6 -> -3;
+            case 5 -> 2;
+            case 4 -> 5;
+            default -> 0;
+        };
+        log.info("evaluate by study days {}", result);
+        return result;
+    }
 
-        for (String[] day : table) {
-            if (dayIsFree(day)) ;
-        }
-        return 0;
+    @Override
+    public int evaluateLoadBalance(String[][] table) {
+        var quantityArr = Arrays.stream(table).map(EvaluationService::getLessonQuantity).toList();
+        int length = quantityArr.size();
+        double u = quantityArr.stream().mapToDouble(i -> i).sum() / length; // Среднее значение
+        double o = Math.sqrt(
+                quantityArr.stream().mapToDouble(i -> Math.pow(i - u, 2)).sum()
+                        / (length - 1)); // Стандартное отклонение
+        double cv = o / u; // Коэффициент вариации
+        int result = -5;
+        if (cv < 0.1) result = 5;
+        if (cv < 0.2) result = 2;
+        if (cv < 0.4) result = 0;
+        if (cv < 0.6) result = -3;
+        log.info("evaluate by load balance with CV = {} and return {}",
+                Math.floor(cv * 100) / 100, result);
+        return result;
     }
 
     @Override
@@ -62,7 +114,15 @@ public class EvaluationService implements EvaluationInterface {
 
     @Override
     public int evaluateWeekendDistribution(String[][] table) {
-        return 0;
+        int result = -2;
+        if (dayIsFree(table[5]) || dayIsFree(table[0])) result = 3;
+        for (int i = 1; i <= table.length - 2; i++) {
+            if (dayIsFree(table[i])) {
+                if (dayIsFree(table[i + 1])) result = 3;
+            }
+        }
+        log.info("evaluate by weekend distribution and return {}", result);
+        return result;
     }
 
     @Override
@@ -75,7 +135,7 @@ public class EvaluationService implements EvaluationInterface {
      * @return Возвращает количество пар в определенный день
      */
     private static int getLessonQuantity(String[] dayTable) {
-        return (int) Arrays.stream(dayTable).filter(str -> !str.equals("nothing")).count();
+        return (int) Arrays.stream(dayTable).filter(EvaluationService::isBlank).count();
     }
     /**
      * @param timeTable полное расписание (на две недели)
@@ -90,7 +150,7 @@ public class EvaluationService implements EvaluationInterface {
      * @return Возвращает true если в этот день нет пар
      */
     private static boolean dayIsFree(String[] dayTable) {
-        return Arrays.stream(dayTable).allMatch(str -> str.equals("nothing"));
+        return Arrays.stream(dayTable).allMatch(EvaluationService::isBlank);
     }
 
     /**
@@ -99,5 +159,23 @@ public class EvaluationService implements EvaluationInterface {
      */
     private static int getFreeDaysQuantity(String[][] timeTable) {
         return (int) Arrays.stream(timeTable).filter(EvaluationService::dayIsFree).count();
+    }
+
+    /**
+     * Делает проверку: str != null && !str.isEmpty();
+     *
+     * @param str строка с информацией о паре
+     * @return true если строка не пуста
+     */
+
+    private static boolean isBlank(String str) {
+        return str != null && !str.isEmpty();
+    }
+
+    public static void main(String[] args) throws IOException {
+        TimeTableParser timeTableParser1 = new TimeTableParser();
+        EvaluationService eva = new EvaluationService(timeTableParser1);
+        var t = timeTableParser1.getTimeTable("/timetable/189115");
+        eva.evaluateTimeTable(t);
     }
 }
